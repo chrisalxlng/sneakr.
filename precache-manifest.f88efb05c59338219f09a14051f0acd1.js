0dca2540de89359e7c263e7fca07a1a731de627f
Christopher Lang <chrsalxlng@gmail.com>
2020-08-29 22:50:27 +0200

    Updates

---
+++ precache-manifest.f88efb05c59338219f09a14051f0acd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bff19a52dce6403326506f62b9322c9",
+    "url": "/sneakr./index.html"
+  },
+  {
+    "revision": "7d368f0e4eab51b696b5",
+    "url": "/sneakr./static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "4906e1f7bfad8820f259",
+    "url": "/sneakr./static/css/main.ff64bb48.chunk.css"
+  },
+  {
+    "revision": "7d368f0e4eab51b696b5",
+    "url": "/sneakr./static/js/2.64ceb4c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sneakr./static/js/2.64ceb4c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4906e1f7bfad8820f259",
+    "url": "/sneakr./static/js/main.b7120417.chunk.js"
+  },
+  {
+    "revision": "1b0b86499213bfb31c98",
+    "url": "/sneakr./static/js/runtime-main.666dbd24.js"
+  }
+]);
